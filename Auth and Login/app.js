75f7db855337209f4ef76177e823591d15868d82
Eslam Mohamed <84045971+Crypt00o@users.noreply.github.com>
2022-05-05 14:13:10 +0200

    Add files via upload

--- Auth and Login/app.js
+++ Auth and Login/app.js
@@ -21,12 +21,16 @@ app.use((req,res,next)=>{
 
 //checking functions
 
-let checkid=({user,pass})=>{
-    if(user=='admin'&&pass=='eslam')
+let checkid=async(obj)=>{
+    let result=await db.LoginUser({username:obj.username,password:obj.password})
+    if(result)
         return true;
     else
         return false;
 }
+//importing my database file
+ let db=require('./database');
+ 
 
 //defineing needed vars
 
@@ -64,10 +68,11 @@ app.get("/login",(req,res)=>{
     
 })
 
-app.post("/login",(req,res)=>{
+app.post("/login",async(req,res)=>{
     let client_cookies=JSON.parse(JSON.stringify(req.cookies));
-  id={user:req.body.user,pass:req.body.pass};
-  if(checkid(id)){
+  id={username:req.body.user,password:req.body.pass};
+  let value=await checkid(id);
+  if(value){
       res.cookie('user',req.body.user);
       res.cookie('login','Yes')
       res.cookie('login_times',(++(client_cookies.login_times)));
@@ -95,8 +100,27 @@ app.post("/home",(req,res)=>{
         res.status(301).redirect("/login");
     }
 })
+app.get("/SignUp",(req,res)=>{
+    res.status(200).sendFile(__dirname+"/public/SignUp.html");
+})
+app.post("/SignUp",async(req,res)=>{
+    let newUser={username:req.body.user,password:req.body.pass};
+    let available=async(newUser)=>{
+        let result=await db.createUser(newUser);
+        return result
+    }
+
+        if(await available(newUser)){
+            res.status(301).redirect("/login")
+        }
+        else{
+            res.status(200).sendFile(__dirname+"/public/re-SignUp.html");
+        }
+    
+   
+})
 app.all("/*",(req,res)=>{
-            res.status(404).send("<h1 style="text-align:center" > Error 404 Not found :(  </h1>");
+            res.status(404).send("<h1 style='text-align:center' > Error 404 Not found :(  </h1>");
 });
 app.listen(port,host,()=>{
     console.log("Listening at %s:%s",host,port)
